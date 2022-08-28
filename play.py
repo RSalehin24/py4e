4b9a9301048515d12ff90ae5595beaff6077c300
Md. Reyanus Salehin <reyanussalehin@iut-dhaka.edu>
2022-08-28 11:37:50 +0600

    previous works are commented in play.py

--- play.py
+++ play.py
@@ -1,40 +1,40 @@
-name = 'Reyanus Salehin'
-print(name.find('a', 4))
+# name = 'Reyanus Salehin'
+# print(name.find('a', 4))
 
-# Dictionary
-dictionary = {'drinks_count' : 10, 'candy_count' : 7}
-print(dictionary.get('candy_count', 0))
+# # Dictionary
+# dictionary = {'drinks_count' : 10, 'candy_count' : 7}
+# print(dictionary.get('candy_count', 0))
 
 
-fruit = 'Banana'
-# fruit[0] = 'b'
-print(fruit)
+# fruit = 'Banana'
+# # fruit[0] = 'b'
+# print(fruit)
 
-#Tuple comparison
-print((2, 2, 8) < (2, 2, 7))
+# #Tuple comparison
+# print((2, 2, 8) < (2, 2, 7))
 
-# sorted dictionary by key
-sorted_dictionary = sorted(dictionary.items())
-print(sorted_dictionary)
+# # sorted dictionary by key
+# sorted_dictionary = sorted(dictionary.items())
+# print(sorted_dictionary)
 
-# sorted dictionary by value
-temp_list = list()
+# # sorted dictionary by value
+# temp_list = list()
 
-# for key, value in dictionary.items():
-#   temp_list.append((value, key))
+# # for key, value in dictionary.items():
+# #   temp_list.append((value, key))
 
-# temp_list = sorted(temp_list, reverse=True) 
+# # temp_list = sorted(temp_list, reverse=True) 
+# # print(temp_list)
+
+# # shorter version
+# temp_list = sorted([(value, key) for key, value in dictionary.items()], reverse=True)
 # print(temp_list)
 
-# shorter version
-temp_list = sorted([(value, key) for key, value in dictionary.items()], reverse=True)
-print(temp_list)
-
-x , y = 3, 4
-print(y)
+# x , y = 3, 4
+# print(y)
 
 
-data = [4, 2, 5, 1, 8]
-data.sort(reverse=True)
+# data = [4, 2, 5, 1, 8]
+# data.sort(reverse=True)
 
-print(data)
+# print(data)
