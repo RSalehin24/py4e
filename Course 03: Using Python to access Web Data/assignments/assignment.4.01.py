9803a38d2af42aefc3ccb7c5d396759adb615640
Md. Reyanus Salehin <reyanussalehin@iut-dhaka.edu>
2022-08-28 23:27:48 +0600

    course03: Week 04: assignements; course02: certificate

--- Course 03: Using Python to access Web Data/assignments/assignment.4.01.py
+++ Course 03: Using Python to access Web Data/assignments/assignment.4.01.py
@@ -1,4 +1,4 @@
-from multiprocessing import context
+from asyncio import constants
 import urllib.request
 from bs4 import BeautifulSoup
 import ssl
@@ -6,3 +6,15 @@ import ssl
 context = ssl.create_default_context()
 context.check_hostname = False
 context.verify_mode = ssl.CERT_NONE
+
+url = input('Enter url: ')
+data = urllib.request.urlopen(url, context = context).read()
+soup = BeautifulSoup(data, 'html.parser')
+spans = soup('span')
+
+total = 0
+
+for span in spans:
+  total += int(span.contents[0])
+  
+print(total)
---
+++ Course 03: Using Python to access Web Data/assignments/assignment.4.02.py
@@ -0,0 +1,28 @@
+from turtle import position
+import urllib.request
+import ssl
+import re
+from bs4 import BeautifulSoup
+
+context = ssl.create_default_context()
+context.check_hostname = False
+context.verify_mode = ssl.CERT_NONE
+
+
+url = input('Enter URL: ')
+count = int(input('Enter count: '))
+position = int(input('Enter position: '))
+print('Retrieving:', url)
+
+for i in range(count):
+  temp_position = position
+  html_data = urllib.request.urlopen(url, context = context).read()
+  soup = BeautifulSoup(html_data, 'html.parser')
+  anchor_tags = soup('a')
+
+  for anchor_tag in anchor_tags:
+    temp_position -= 1
+    if temp_position == 0:
+      url = anchor_tag.get('href', None)
+  
+  print('Retrieving:', url)
