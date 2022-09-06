ca7034c9934054393bcc310d3d44696a11e52b4c
Md. Reyanus Salehin <reyanussalehin@iut-dhaka.edu>
2022-09-06 11:19:50 +0600

    unfinished: course 04: week 02

---
+++ Course 04: Using Databases with Python/topics/email_db.py
@@ -0,0 +1,37 @@
+import sqlite3
+
+connection = sqlite3.connect("emaildb.sqlite")
+cursor = connection.cursor()
+
+cursor.execute('''
+DROP TABLE IF EXISTS COUNTS
+''')
+cursor.execute('''
+CREATE TABLE COUNTS (email TEXT, count INTEGER)
+''')
+
+file_name = input("Enter file name: ")
+file_handle = open(file_name)
+
+for line in file_handle :
+  if not line.startswith()("From: ") : continue
+  substring = line.split()
+  email = substring[1]
+  cursor.execute('SELECT count FROM COUNTS WHERE email = ?', (email,))
+  row = cursor.fetchone()
+  if row is None  :
+    cursor.execute('''
+    INSERT INTO COUNTS (email, count) VALUES (?, 1)               
+    ''', (email,))
+  else :
+    cursor.execute('''
+    UPDATE COUNTS SET count = count + 1 WHERE email = ?                     
+    ''', (email,))
+    
+  connection.commit()
+
+sql_get_top_ten_emails = "SELECT email, count FROM COUNTS ORDER BY count DESC LIMIT 10"
+
+# for row in cursor.execute(sql_get_top_ten_emails) :
+  
+  
